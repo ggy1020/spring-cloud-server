eefb419e007b17e43d1c0ca8b403d34282db9368
gaoguangyao <15255626300@163.com>
2019-01-08 17:43:48 +0800

    初始提交

--- src/main/java/com/ggy/springbootserver/SpringBootServerApplication.java
+++ src/main/java/com/ggy/springbootserver/SpringBootServerApplication.java
@@ -1,13 +1,27 @@
 package com.ggy.springbootserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBootServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootServerApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("/hi")
+	public String getPort(){
+		return port;
 	}
 
 }
